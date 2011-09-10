8952f61b6e15053b63dc9412f93cd21d2276d755
Manish Manjappa Yathnalli <manish@dashidumma.(none)>
2011-09-10 11:15:15 +0530

    removed not working libs and added some of my own

---
+++ manish.el
@@ -0,0 +1,7 @@
+;(add-to-list 'load-path "~/.emacs.d/slime/")  ; your SLIME directory
+;(setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
+;(require 'slime)
+;(require 'slime-autoloads)
+;(slime-setup)
+;(slime-setup '(slime-scratch slime-editing-commands))
+;(slime-setup '(slime-repl)) ; almost everything
